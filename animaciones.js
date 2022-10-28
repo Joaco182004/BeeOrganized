e8336d59fdebb3223fad0a6d6abea2268183f6e7
HubCJ <u20201b742@upc.edu.pe>
2022-10-27 22:57:26 -0500

    lista de actividades realizado

--- animaciones.js
+++ animaciones.js
@@ -7,19 +7,16 @@ sr.reveal('.sobre-nosotros',{
     duration:3000,
     origin:'left',
     distance:'-300px',
-
 });
 sr.reveal('.servicios',{
     duration:3000,
     origin:'rigth',
     distance:'-300px',
-
 });
 sr.reveal('.app',{
     duration:3000,
     origin:'left',
     distance:'-300px',
-
 });
 
 sr.reveal('.soporte',{
